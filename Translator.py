d6e965ed553c8889e435ec2e64222791a71c32f0
Тимофей Привалов <Tima.privalov@bk.ru>
2023-05-17 14:26:05 +0300

    Я точно закончил

--- Translator.py
+++ Translator.py
@@ -5,11 +5,9 @@ class Translator_bot:
     @staticmethod
     def translate(message):
         text = message.lower()
-        print(text)
         text = text.split(' ')
         text.pop(0)
         translator = Translator(from_lang=text[0], to_lang=text[len(text)-1])
-        print(text)
         text.pop(0)
         text.pop(len(text)-1)
         return translator.translate(" ".join(text))
